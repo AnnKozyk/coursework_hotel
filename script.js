c1da56d3f0d8909077fe4d165ac7b19f8f330d78
Anna <145040688+AnnKozyk@users.noreply.github.com>
2024-05-17 22:54:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const callButton = document.getElementById("callButton");
 
 callButton.addEventListener("click", function() {
   // Replace "your-phone-number" with your actual phone number
-  window.open("tel:+1800229933");
+  window.open("tel:+380995556799");
 });
