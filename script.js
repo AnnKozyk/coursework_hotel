ba0b56f7113bf4defa80f306ec882fa8e83407d2
Anna <145040688+AnnKozyk@users.noreply.github.com>
2024-05-17 21:34:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const callButton = document.getElementById("callButton");
+
+callButton.addEventListener("click", function() {
+  // Replace "your-phone-number" with your actual phone number
+  window.open("tel:+1800229933");
+});
